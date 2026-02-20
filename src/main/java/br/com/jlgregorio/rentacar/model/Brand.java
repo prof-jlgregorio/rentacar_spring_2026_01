d814bd51e97d4d63fa96fcf7bc31247cde384e6c
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2026-02-20 09:16:28 -0300

    Model (Brands) and Database are OK!

---
+++ src/main/java/br/com/jlgregorio/rentacar/model/Brand.java
@@ -0,0 +1,40 @@
+package br.com.jlgregorio.rentacar.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private Date updatedAt;
+
+
+
+}
